7ad13480ce64f1ba309769065553b8b8e21ac0d1
itzRoma <romabondarenko03@gmail.com>
2023-01-24 19:33:46 +0200

    project init with spring initializr

---
+++ src/main/java/com/itzroma/astrocornerapi/AstrocornerApiApplication.java
@@ -0,0 +1,13 @@
+package com.itzroma.astrocornerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrocornerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstrocornerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itzroma/astrocornerapi/AstrocornerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzroma.astrocornerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrocornerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
